65ee787d27b8f618d3f7d639766ccc9c49ec51ac
BillStephens2022 <two4onebill@yahoo.com>
2023-12-13 22:23:17 -0500

    updated home page and about page with more content

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,15 +1,54 @@
 import Header from "@/components/layout/header";
+import Button from "@/components/ui/Button";
 import classes from "@/pages/about/about.module.css";
+import Link from "next/link";
 
 const About = () => {
-    return (
-        <>
-          <Header pageTitle="About Us"></Header>
-          <main className={classes.main}>
-
-          </main>
-        </>
-    );
-}
+  return (
+    <>
+      <Header pageTitle="About Us"></Header>
+      <div className={classes.about_nav}>
+        <div className={classes.about_nav_item}>
+          <h3 className={classes.about_title}>Our Story</h3>
+          <p className={classes.about_p}>
+            In 2017 the Kumdo @ KIPP program began at The KIPP Cooper Norcross
+            Academy in Camden, NJ. This program provides free training in the
+            very exclusive martial art of Kumdo (more commonly known as Kendo)
+            to KIP&apos;s young Lanning Square Middle School students. This program
+            had been heralded in publications such as The Philadelphia Inquirer:&nbsp;&nbsp;&nbsp;
+              <Link href="https://www.philly.com/philly/news/new_jersey/its-called-the-golf-of-martial-arts-but-this-camden-school-is-making-kumdo-more-inclusive-20180331.html">It's Called the Golf of Martial Arts</Link>
+          
+            
+          </p>
+          <p className={classes.about_p}>
+            The first year of the program was a great success and was
+            subsequently expanded at the request of the KIPP Academy to include
+            primary students. It was soon identified that graduating KIPP
+            Lanning Square Middle School scholars would no longer have the
+            ability to train in Kumdo under the Kumdo @ KIPP program. It was
+            also identified that in order to provide the fullest Kumdo training
+            experience to Kumdo @ KIPP students, said students would need to
+            train at greater frequencies and at the SYK Sword Academy of Cherry
+            Hill, NJ.{" "}
+          </p>{" "}
+          <p className={classes.about_p}>
+            This led to the genesis of this Not For Profit—Enter The Johnsons.
+            Enter The Johnsons (ETJ) raises funds to financially sponsor Kumdo
+            training for Kumdo @ KIPP Program students and alumni. Eventually,
+            ETJ will provide other historically challenged youth in the Delaware
+            Valley Region, primarily of The City of Camden, that are not Kumdo @
+            KIPP Program alumni, the funding and support to train in some type
+            of quality martial art. ETJ will continue the important work that
+            was started.
+          </p>
+        </div>
+        <div className={classes.about_nav_item}>
+          <h3 className={classes.about_title}>Our Team</h3>
+        </div>
+      </div>
+      <main className={classes.main}></main>
+    </>
+  );
+};
 
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -28,28 +28,39 @@ const Home = () => {
       </div>
       <Header>ETJ</Header>
       {/* <div className={classes.background_image_container}></div> */}
-      <div className={classes.donate_button_div}>
-        <Button
-          backgroundImage="var(--linear-gradient-red)"
-          href="https://www.mightycause.com/story/Iasxuf"
-          className={classes.donate_button}
-        >
-          Donate
-        </Button>
-      </div>
       <main className={classes.main}>
         <h3 className={classes.main_heading}>
           Martial arts and mentoring for our kids who need it most.
         </h3>
-      
+        <p className={classes.main_p}>We&#39;re a 501c3 - EIN: 83-2454419</p>
+
         <div className={classes.main_container}>
           <div className={classes.row}>
             <div className={classes.box}>
-              <h4 className={classes.box_header}>About Us</h4>
-              <p className={classes.box_p}>We&#39;re a 501c3 - EIN: 83-2454419</p>
+              <h4 className={classes.box_header}>Our Vision</h4>
+              <p className={classes.box_p}>
+                To be an effective Martial Art benefactor by providing quality
+                Martial Arts training for youth that are historically challenged
+                by ethnicity, financial means and demography within the Delaware
+                Valley Region and beyond.
+              </p>
             </div>
             <div className={classes.box}>
               <h4 className={classes.box_header}>Fundraising</h4>
+              <p className={classes.box_p}>
+                ETJ has partnered with Mightycause to provided funding for this
+                unique initiative. Please help us.
+              </p>
+              <p className={classes.box_p_support}>Support us today!</p>
+              <div className={classes.donate_button_div}>
+                <Button
+                  backgroundImage="var(--linear-gradient-red)"
+                  href="https://www.mightycause.com/story/Iasxuf"
+                  className={classes.donate_button}
+                >
+                  Donate
+                </Button>
+              </div>
             </div>
           </div>
           <div className={classes.row}>
