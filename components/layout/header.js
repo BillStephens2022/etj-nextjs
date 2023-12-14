5a6543a9e37868d37ad250af71d7d6d75f9289b2
BillStephens2022 <two4onebill@yahoo.com>
2023-12-14 10:12:36 -0500

    improved responsiveness in header component

--- components/layout/header.js
+++ components/layout/header.js
@@ -4,7 +4,7 @@ import classes from "@/components/layout/header.module.css";
 const Header = ({ pageTitle }) => {
   return (
     <div className={classes.header_container}>
-      <h1 className={classes.header}><GiCrossedSwords /> ETJ <GiCrossedSwords /></h1>
+      <h1 className={classes.header}><span className={classes.swords}><GiCrossedSwords /></span> ETJ <span className={classes.swords}><GiCrossedSwords /></span></h1>
       {pageTitle && <h2 className={classes.page_heading}>{pageTitle}</h2>}
     </div>
   );
