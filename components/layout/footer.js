713552e43c388a11b0a52f705512395fae3919c3
BillStephens2022 <two4onebill@yahoo.com>
2023-12-17 11:31:06 -0500

    fixed font sizes and alignment in footer and adjusted for smaller screens

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,14 +1,29 @@
 import Link from "next/link";
 import { GoPasskeyFill } from "react-icons/go";
+import { FaSquareFacebook } from "react-icons/fa6";
 import classes from "@/components/layout/footer.module.css";
 
 const Footer = () => {
   return (
     <div className={classes.footer}>
-      <Link href="/admin"><div className={classes.login_icon}><GoPasskeyFill fill="white" size="16px" /></div></Link>
-      <p className={classes.text}>Copyright © 2023, ETJ, a Non-Profit 501(c)(3) organization.&nbsp;&nbsp;Designed by: <Link className={classes.link} href="https://billstephens2022.github.io/my_portfolio/">Bill Stephens</Link>. All Rights Reserved.</p>
+      <Link href="/admin">
+        <div className={classes.login_icon}>
+          <GoPasskeyFill fill="white" size="16px" />
+        </div>
+      </Link>
+      <p className={classes.text}>
+        Copyright © 2023, ETJ, a Non-Profit 501(c)(3) organization.&nbsp;&nbsp;{" "}
+        Designed by:{" "}
+        <Link
+          className={classes.link}
+          href="https://billstephens2022.github.io/my_portfolio/"
+        >
+          Bill Stephens
+        </Link>
+        .&nbsp;&nbsp;All Rights Reserved.
+      </p>
     </div>
   );
-}
+};
 
 export default Footer;
